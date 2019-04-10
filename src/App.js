bbbf5a281c649b5822d4f1885c7b4a6f0c7c7067
Maciej Matuszczak <maciej.matuszczak96@gmail.com>
2019-04-10 20:11:39 +0200

    test

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
-            <div>dsf</div>
+            <div>asdasddsf</div>
           </p>
           <a
             className="App-link"
